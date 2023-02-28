e697c9633493b6c302f576c07403296048fdf160
Rozzku <ferguso2478@gmail.com>
2023-02-28 09:29:32 +0800

    Add: Laporan

--- pembayaran/pembayaran.php
+++ pembayaran/pembayaran.php
@@ -25,12 +25,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Pembayaran</title>
+    <title>Data SPP</title>
     <link rel="stylesheet" href="../CSS/style.css">
 </head>
 
 <body>
-    <h1>Data Pembayaran</h1>
+    <h1>Data SPP</h1>
 
     <form action="" method="GET" autocomplete="off" class="search-form">
         <?php if (!isset($_GET['keyword'])) : ?>
--- spp/laporan.php
+++ spp/laporan.php
@@ -1,6 +1,5 @@
 <?php 
     require 'functions_spp.php';
-    require '../views/template/navbar.php';
     $spp = query("SELECT * FROM tb_spp INNER JOIN tb_siswa USING (nis) INNER JOIN tb_petugas USING (nip) ORDER BY id_spp DESC")
 ?>
 
@@ -10,11 +9,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Histori Pembayaran</title>
+    <title>Laporan</title>
     <link rel="stylesheet" href="../CSS/style.css">
 </head>
 <body>
-    <h1>Histori Pembayaran</h1>
+<h1>Laporan Pembayaran</h1>
     <table border="1" cellspacing="0" cellpadding="10">
         <tr>
             <th>Petugas</th>
@@ -34,5 +33,17 @@
         </tr>
         <?php endforeach; ?>
     </table>
+
+    <div class="box-ttd">
+            <p>Denpasar, <?= date('d-m-Y'); ?></p>
+            <p>Gungde Solahudin</p>
+        </div>
+        <div class="layer"></div>
+    </div>
+
+    <script>
+        window.print();
+        window.onafterprint = () => history.back();
+    </script>
 </body>
 </html>
