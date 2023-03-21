54bdb298c38671fe2e11649543ab88815ef93104
Mezzky <ryansahadha@gmail.com>
2023-03-21 10:07:38 +0800

    Delete: Dead Code

--- spp/laporan_kelas.php
+++ spp/laporan_kelas.php
@@ -12,15 +12,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laporan Kelas</title>
-    <style>
-        /* table,
-        tr,
-        th,
-        td {
-            border: 1px solid black;
-            border-collapse: collapse;
-        } */
-    </style>
 </head>
 
 <body>
